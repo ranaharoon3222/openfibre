bf788574d5165379ab4ad9f05aeb545ca550e3e4
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-05-20 12:29:19 +0500

    done

--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
   const authorization = req.headers.authorization;
 
   try {
-    const resp = await axios.get(
+    const resp = await axios.post(
       'https://api.ms3.net/api/V1/FTTPOrders/Create',
       {
         headers: {
