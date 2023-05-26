c92925f9e66689afb8420d1102f515d4f62044be
ranaharoon3222 <ranaharoon3222@gmail.com>
2023-05-26 16:34:40 +0500

    add deals

--- src/pages/api/codg.js
+++ src/pages/api/codg.js
@@ -26,6 +26,22 @@ export default async function handler(req, res) {
   const authorization = req.headers.authorization;
   console.log(bodyData);
   const rData = JSON.parse(bodyData);
+  let productId;
+  let productPrice;
+
+  if (rData.fttpProductCode == 'F_A_L3_100') {
+    productId = 392;
+    productPrice = 28.99 * 100;
+  } else if (rData.fttpProductCode == 'F_A_L3_1000') {
+    productId = 394;
+    productPrice = 38.99 * 100;
+  } else if (rData.fttpProductCode == 'F_A_L3_500') {
+    productId = 393;
+    productPrice = 32.99 * 100;
+  } else if (rData.fttpProductCode == 'F_E_L3_100') {
+    productId = 395;
+    productPrice = 22 * 100;
+  }
 
   try {
     const resp = await fetch('https://api.ms3.net/api/V1/FTTPOrders/Create', {
@@ -37,8 +53,91 @@ export default async function handler(req, res) {
       body: JSON.stringify(rData),
     });
 
-    const lData = await resp.json();
-    res.status(200).json(lData);
+    const meetingOrder = await resp.json();
+
+    const contactResp = await fetch(
+      'https://openvoipfibre.eu.teamwork.com/crm/api/v2/deals.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer tkn.v1_NTI1MzNlNjItYzRhOS00NTg0LWI4NGItYThiMmI4NGMyZGI1LTk4ODQxOS40ODE1NzcuRVU=',
+        },
+        body: JSON.stringify({
+          contact: {
+            addressLine1: rData.onSiteLocation,
+            emailAddresses: [
+              {
+                address: rData.subscriberContactEmail,
+                isMain: true,
+              },
+            ],
+            firstName: rData.primarySiteContactName,
+            lastName: '',
+            phoneNumbers: [
+              {
+                isMain: true,
+                number: rData.subscriberContactNumber,
+              },
+            ],
+            title: rData.primarySiteContactName,
+            zipcode: rData.uprn,
+          },
+        }),
+      }
+    );
+
+    const contactData = await contactResp.json();
+
+    const dealResp = await fetch(
+      'https://openvoipfibre.eu.teamwork.com/crm/api/v2/deals.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer tkn.v1_NTI1MzNlNjItYzRhOS00NTg0LWI4NGItYThiMmI4NGMyZGI1LTk4ODQxOS40ODE1NzcuRVU=',
+        },
+        body: JSON.stringify({
+          deal: {
+            expectedCloseDate: `${rData.customerRequestedDate}T14:15:22Z`,
+            stage: {
+              id: 14498,
+              type: 'stages',
+            },
+            products: [
+              {
+                id: productId,
+                meta: {
+                  discount: 0,
+                  price: productPrice,
+                  quantity: 1,
+                },
+                type: 'products',
+              },
+            ],
+            contacts: [
+              {
+                id: contactData.contact.id,
+                meta: {
+                  isMain: true,
+                },
+                type: 'contacts',
+              },
+            ],
+            state: 'open',
+            title: rData.primarySiteContactName,
+          },
+        }),
+      }
+    );
+
+    const dealData = await dealResp.json();
+
+    console.log(dealData);
+
+    res.status(200).json({ meetingOrder, dealData, contactData });
   } catch (error) {
     res.status(400).json(error);
   }
